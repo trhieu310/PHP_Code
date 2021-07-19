08826b3970c8c9a26f09b7982f5d1ffce24163d7
trhieu310 <58017675+trhieu310@users.noreply.github.com>
2021-07-19 16:06:02 +0700

    add bai 12

--- My_code/bai12/index.php
+++ My_code/bai12/index.php
@@ -1,22 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Rating</title>
-	<meta charset="utf-8">
-	<link href="template/css.css" type="text/css" rel="stylesheet">
-</head>
-<body>
-	<div id="container">
-		<header>
-			<h1><a href="index.php">COMPUTER ABC</a></h1>
-		</header>
+<?php
+    include("functions.php");
 
-		<div id="main-wrapper">
-			<?php echo $html ?>
-		</div>
+    if(isset($_GET['id'])){
+        $id=$_GET['id'];
+        // UPDATE RATING
+        if(isset($_POST['rate_submit'])){
+            if(isset($_POST['rate'])){
+                $rate = $_POST['rate'];
+                setRatingInfo($id, $rate);
+            }
+        }
 
-		<footer>
-		</footer>
-	</div>
-</body>
-</html>
+        $html = getProduct($id);
+        include("template/view-product.php");
+    }else{
+        $html = listProduct();
+        include("template/list-products.php");
+    }
+
+?>
